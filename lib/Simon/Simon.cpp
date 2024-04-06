e2ea2095e200927b1c552381947a8bf2a2a47c5d
mat-mal <mateusz2005_1990@tlen.pl>
2024-04-06 16:58:00 +0200

    add computer opponent

--- lib/Simon/Simon.cpp
+++ lib/Simon/Simon.cpp
@@ -4,7 +4,8 @@ Simon simon;
 
 Simon::Simon()
 {
-    counter = 0;
+    countStages = 0;
+    countSteps = 0;
     simonLights_timer = 0;
     redOn = false;
     yellowOn = false;
@@ -14,15 +15,31 @@ Simon::Simon()
 
 void Simon::SimonLights()
 {
-    if(counter < simonLimit && Timer(interval, &simonLights_timer))
+    if(countStages < difficultyLevel && Timer(interval, &simonLights_timer))
     {
-        lightsOrder[counter] = random(Light_RED, Light_BLUE + 1);
-        if(lightsOrder[counter] == Light_RED) redOn = true;
-        if(lightsOrder[counter] == Light_YELLOW) yellowOn = true;
-        if(lightsOrder[counter] == Light_GREEN) greenOn = true;
-        if(lightsOrder[counter] == Light_BLUE) blueOn = true;    
-        counter++;
+        Serial.print(countStages);
+        if(countSteps < (countStages + 1))
+        {
+            Serial.print(countSteps);
+            series[countStages] = random(Light_RED, Light_BLUE + 1);
+            stages[countStages][countSteps] = series[countSteps];
+            Serial.print(" ");
+            Serial.println(stages[countStages][countSteps]);
+            if(stages[countStages][countSteps] == Light_RED) redOn = true;
+            if(stages[countStages][countSteps] == Light_YELLOW) yellowOn = true;
+            if(stages[countStages][countSteps] == Light_GREEN) greenOn = true;
+            if(stages[countStages][countSteps] == Light_BLUE) blueOn = true;   
+            countSteps++;
+
+        }
+        if((countStages + 1) == countSteps)
+        {
+            countStages++;
+            countSteps = 0;
+        }
+        
     }
+    // Serial.println(stages[countStages][countSteps]);
     Red.ToggleLight(&redOn);
     Yellow.ToggleLight(&yellowOn);
     Green.ToggleLight(&greenOn);
--- lib/Simon/Simon.h
+++ lib/Simon/Simon.h
@@ -3,14 +3,17 @@
 #include <Arduino.h>
 #include "Interface.h"
 
-const int simonLimit = 10;
+const int difficultyLevel = 10;                     //how many light will be on last stage
+const int maxSteps = 10;                            //step - playing one note/push button once
 
 class Simon
 {
 private:
     int lights_PIN;
-    int lightsOrder[simonLimit];
-    int counter;
+    int series[difficultyLevel];                    //full series of lights
+    int stages[difficultyLevel][maxSteps] = {0};
+    int countSteps;
+    int countStages;
     int interval = 600;
     unsigned long simonLights_timer;
     bool redOn;
